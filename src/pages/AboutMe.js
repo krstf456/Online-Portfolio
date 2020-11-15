4ea233dd229ca7a63ac7e8d8ab36525331bb448e
Kristóf <krstf456@gmail.com>
2020-11-15 22:36:46 +0100

    3.faq

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,11 +1,13 @@
 import AboutSection from "../components/AboutSection"
 import ServicesSection from "../components/ServicesSection"
+import FaqSection from "../components/FaqSection"
 
 const AboutMe = () => {
     return(
         <>
         <AboutSection/>
         <ServicesSection/>
+        <FaqSection/>
         </>
     )
 }
