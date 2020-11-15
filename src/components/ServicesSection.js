9671454ba722bad3b293e1805eb33c2f248495a2
Kristóf <krstf456@gmail.com>
2020-11-15 22:24:16 +0100

    2.services

---
+++ src/components/ServicesSection.js
@@ -0,0 +1,49 @@
+import clock from '../img/clock.svg'
+import diagraph from '../img/diaphragm.svg'
+import money from '../img/money.svg'
+import teamwork from '../img/teamwork.svg'
+import Home2 from '../img/home2.png'
+const ServicesSection = () => {
+    return(
+        <div className="services">
+            <div className="description">
+                <h2>High<span>quality</span>services</h2>
+                <div className="cards">
+                    <div className="card">
+                        <div className="icon">
+                            <img src={clock} alt="clock"/>
+                            <h3>Efficient</h3>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div className="card">
+                        <div className="icon">
+                            <img src={teamwork} alt="teamwork"/>
+                            <h3>Teamwork</h3>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div className="card">
+                        <div className="icon">
+                            <img src={diagraph} alt="dia"/>
+                            <h3>Diapghram</h3>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div className="card">
+                        <div className="icon">
+                            <img src={money} alt="money"/>
+                            <h3>Affordable</h3>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="img">
+                <img src={Home2} alt="camera"/>
+            </div>
+        </div>
+    )
+}
+
+export default ServicesSection
