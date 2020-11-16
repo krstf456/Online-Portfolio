82bcac720da81dfe99d96f0d62eb3b2653f38c88
Kristóf <krstf456@gmail.com>
2020-11-16 10:47:42 +0100

    nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import GlobalStyle from './components/GlobalStyled'
 import AboutMe from './pages/AboutMe';
-
+import Nav from './components/Nav'
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
+      <Nav/>
       <AboutMe/>
     </div>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+
+const Nav = () => {
+    return(
+        <StyledNav>
+            <h1><a id="logo" href="#">Portfolio</a></h1>
+            <ul>
+                <li>
+                    <a href="#">1. About me</a>
+                </li>
+                <li>
+                    <a href="#">2. My Work</a>
+                </li>
+                <li>
+                    <a href="#">3. Contact Me</a>
+                </li>
+            </ul>
+        </StyledNav>
+    )
+}
+
+const StyledNav = styled.nav`
+    min-height: 10vh;
+    display: flex;
+    margin: auto;
+    justify-content: space-between;
+    align-items:center;
+    padding: 1rem 10rem;
+    background: #282828;
+    a{
+        color: white;
+        text-decoration: none;
+    }
+    ul{
+        display: flex;
+        list-style: none;
+    }
+    #logo{
+        font-family: "Lobster", cursive;
+        font-weight: lighter;
+        font-size: 1.5rem;
+    }
+    li{
+        padding-left: 10rem;
+        position: relative;
+    }
+`
+
+export default Nav
