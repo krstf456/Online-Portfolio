9113d2ffe6a2b92639a9040a861042f4406f1833
Kristóf <krstf456@gmail.com>
2020-11-16 13:39:31 +0100

    framer motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,20 +1,35 @@
 import Home1 from '../img/home1.png'
 import { About, Description, Image, Hide } from '../styles'
+
+//framer motion
+
+import {motion} from 'framer-motion'
+
 const AboutSection = () => {
+
+
+    // const titleAnimation = {
+    //     hidden: {opacity: 0},
+    //     show: {opacity: 1, transition: {duration: 2}}
+    // }
+    // const container = {
+    //     hidden: {x: 100},
+    //     show: {x: 0, transition: {duration: 0.75, ease: "easeOut", staggerChildren: 1, when: "afterChildren"}}
+    // }
     return(
         <About>
             <Description>
-                <div className="title">
+                <motion.div className="title">
                     <Hide>
-                        <h2>My goal is to make</h2>
+                        <motion.h2>My goal is to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>your<span>dreams</span> come</h2>
+                        <motion.h2>your<span>dreams</span> come</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>true.</h2>
+                        <motion.h2>true.</motion.h2>
                     </Hide>
-                </div>
+                </motion.div>
                 <p>Contact me for more information</p>
                 <button>Contact me</button>
             </Description>
