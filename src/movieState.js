34ef0d9a6818e21d466506cdf72e6bb49f14b80b
Kristóf <krstf456@gmail.com>
2020-11-17 10:59:22 +0100

    dl

--- src/movieState.js
+++ src/movieState.js
@@ -1,10 +1,5 @@
 //Import Images
-import athlete from "./img/athlete-small.png";
-import goodtimes from "./img/goodtimes-small.png";
-import theracer from "./img/theracer-small.png";
-import athlete2 from "./img/athlete2.png";
-import goodtimes2 from "./img/good-times2.jpg";
-import theracer2 from "./img/the-racer2.jpg";
+
 import mainhstore from "./img/mainhstore.png"
 import secondaryhstore from "./img/secondaryhstore.png"
 
